5b10b895391f007fab06be2c58615c9e27c9dd94
Pratham Kashyap <kashyappratham000@gmail.com>
2025-05-30 07:05:07 +0000

    5th

--- models/stg_customer.sql
+++ models/stg_customer.sql
@@ -1,4 +1,4 @@
-    customers as (
+with customers as (
         select id as customer_id, first_name, last_name
         from {{ source("jaffle_shop", "customers") }}
     )
--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -1,4 +1,4 @@
- orders as (
+with orders as (
         select id as order_id, user_id as customer_id, order_date, status
         from {{ source("jaffle_shop", "orders") }}
     )
